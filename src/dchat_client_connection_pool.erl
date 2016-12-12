ee8a6f918ee1ede95e557848b88f69f8e771f4b5
Mateusz "maxmati" Nowotyński <admin@maxmati.pl>
2016-12-12 23:29:24 +0100

    Removed unnecessary comments

--- src/dchat_client_connection_pool.erl
+++ src/dchat_client_connection_pool.erl
@@ -30,14 +30,6 @@ get_any_connection() ->
   [{_, Pid, _, _}| _] = supervisor:which_children(?SERVER),
   Pid.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the supervisor
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(start_link() ->
-  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
 start_link() ->
   supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
@@ -45,23 +37,6 @@ start_link() ->
 %%% Supervisor callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Whenever a supervisor is started using supervisor:start_link/[2,3],
-%% this function is called by the new process to find out about
-%% restart strategy, maximum restart frequency and child
-%% specifications.
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(init(Args :: term()) ->
-  {ok, {SupFlags :: {RestartStrategy :: supervisor:strategy(),
-    MaxR :: non_neg_integer(), MaxT :: non_neg_integer()},
-    [ChildSpec :: supervisor:child_spec()]
-  }} |
-  ignore |
-  {error, Reason :: term()}).
 init([]) ->
   RestartStrategy = simple_one_for_one,
   MaxRestarts = 1000,
