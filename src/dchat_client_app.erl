96daad797d01d06f86dc457f80ad6b133fcd47ff
Mateusz "maxmati" Nowotyński <admin@maxmati.pl>
2017-01-08 18:13:00 +0100

    Disconnect and send

--- src/dchat_client_app.erl
+++ src/dchat_client_app.erl
@@ -8,7 +8,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([connect/2, start/2, stop/1]).
+-export([connect/2, send/2, disconnect/0, start/2, stop/1]).
 
 %%====================================================================
 %% API
@@ -17,6 +17,13 @@ connect(Servers, Nickname) ->
     application:start(dchat_client),
     lists:foreach(fun dchat_client_connection_manager:connect/1, Servers),
     dchat_client_connection_manager:send({login, [Nickname]}).
+
+send(Recipient, Message) ->
+    dchat_client_connection_manager:send({message, [Recipient, Message]}).
+
+disconnect() ->
+    dchat_client_connection_manager:send({logout, []}),
+    application:stop(dchat_client).
 
 start(_StartType, _StartArgs) ->
     dchat_client_sup:start_link().
